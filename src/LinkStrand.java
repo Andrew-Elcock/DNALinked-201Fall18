f015d72dd6db71e9065064c48343c3da4249d13f
Andrew Elcock <andrew.elcock@duke.edu>
2018-10-26 18:24:18 -0400

    meh

--- src/LinkStrand.java
+++ src/LinkStrand.java
@@ -66,37 +66,71 @@ public class LinkStrand implements IDnaStrand {
 		return this;
 	}
 
+//	@Override
+//	public IDnaStrand reverse() {
+//		Node cur = myFirst;
+//		StringBuilder tempsb = new StringBuilder(myFirst.info);
+//		tempsb.reverse();
+//		Node n = new Node(tempsb.toString());
+//		if(cur.next != null) {
+//			cur = cur.next;
+//		}
+//		while(cur.next != null) {
+//			Node temp = new Node(cur.info);
+//			temp.next = n;
+//			StringBuilder tempsb2 = new StringBuilder(temp.info);
+//			tempsb2.reverse();
+//			temp.info = tempsb2.toString();
+//			n = temp;
+//			cur = cur.next;
+//		}
+//		Node first = n;
+//		LinkStrand ret = new LinkStrand(first.info);
+//		if(first.next != null) {
+//			first = first.next;
+//		}
+//		while(first.next != null) {
+//			ret.append(first.info);
+//			first = first.next;
+//		}
+//		//ret.append(first.info);
+//		return ret;
+//	}
+
+
 	@Override
 	public IDnaStrand reverse() {
 		Node cur = myFirst;
-		StringBuilder tempsb = new StringBuilder(myFirst.info);
-		tempsb.reverse();
-		Node n = new Node(tempsb.toString());
-		if(cur.next != null) {
-			cur = cur.next;
+		Node head = myFirst;
+		Node n = new Node("");
+		if(cur.next == null) {
+			StringBuilder tempsb = new StringBuilder(cur.info);
+			n.info = tempsb.reverse().toString();
 		}
 		while(cur.next != null) {
-			Node temp = new Node(cur.info);
-			temp.next = n;
-			StringBuilder tempsb2 = new StringBuilder(temp.info);
-			tempsb2.reverse();
-			temp.info = tempsb2.toString();
-			n = temp;
+			StringBuilder tempsb = new StringBuilder(cur.info);
+			Node run = new Node(tempsb.reverse().toString());
+			run.next = n;
+			n = run;
 			cur = cur.next;
 		}
-		Node first = n;
-		LinkStrand ret = new LinkStrand(first.info);
-		if(first.next != null) {
-			first = first.next;
+		if(head.next != null) {
+			StringBuilder tempsb = new StringBuilder(cur.info);
+			Node n2 = new Node(tempsb.reverse().toString());
+			n2.next = n;
+			n = n2;
 		}
-		while(first.next != null) {
-			ret.append(first.info);
-			first = first.next;
+		LinkStrand ret = new LinkStrand(n.info);
+		if(n.next != null) {
+			n = n.next;
 		}
-		ret.append(first.info);
+		while(n.next != null) {
+			ret.append(n.info);
+			n = n.next;
+		}
 		return ret;
-	}
-
+	}	
+	
 	@Override
 	public int getAppendCount() {
 		return myAppends;
